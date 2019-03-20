1315d249b5536e44d8f2d1ceaf3e5f37a15111e5
vectorwings <vectorwings@163.com>
2019-03-20 21:59:25 +0800

    finish drag-and-drop

--- renderer/index.js
+++ renderer/index.js
@@ -1,15 +1,31 @@
-const fs = require('fs');
-window.onload = function() {
-    const btn = this.document.querySelector('#btn');
-    const textarea = this.document.querySelector('#textarea');
+/**
+ * html5 拖放 api：http://www.runoob.com/html/html5-draganddrop.html
+ * 
+ * 
+ * 
+ */
 
-    btn.onclick = function() {
-        /**
-         * 获取本地文件
-         * 赋值给 textarea
-         */
-        fs.readFile('package.json', (err, data) => {
-            textarea.innerHTML = data;
-        })
-    }
+const fs = require('fs'); 
+const content = document.querySelector('#content');
+
+content.ondragenter = content.ondragover = content.ondragleave = function() {
+	// 阻止默认行为  
+	return false;
+}
+
+content.ondrop = function(e) {
+	// 阻止默认行为
+	e.preventDefault();
+	console.log(e);
+	console.log(e.dataTransfer.files[0]);
+	const path = e.dataTransfer.files[0].path;
+
+	fs.readFile(path, 'utf-8', (err, data) => {
+		if (err) {
+			console.log(err);
+			return false;
+		} else {
+			content.innerHTML = data;
+		}
+	});
 }

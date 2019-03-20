d4b93eb9eb610c55c9d106c8d74f349ece467618
vectorwings <vectorwings@163.com>
2019-03-20 22:55:23 +0800

    fix

--- main.js
+++ main.js
@@ -1,39 +1,56 @@
-// 主进程
+//主进程
 
-// 引入 electron 模块
-const electron = require('electron');
+// 引入electron模块
+const electron = require("electron");
 
-const path = require('path');
+// nodejs中的path模块
+const path = require("path");
 
-// 创建 electron 引用
+// 创建electron引用
+// 控制应用生命周期的模块
 const app = electron.app;
 
-// 创建 electron BroswerWindow 的应用
+// 创建electron BrowserWindow的引用
+// 窗口相关的模块
 const BrowserWindow = electron.BrowserWindow;
 
-// 变量 保存对应窗口的引用
+// 变量 保存对应用窗口的引用
 let mainWindow = null;
 
-app.on('ready', () => {
-    // 创建 BroswerWindow 的实例 赋值给 win 打开窗口
+function createWindow() {
+  //创建BrowserWindow的实例 赋值给mainWindow打开窗口
+  //软件默认打开的宽度高度 {width:400,height:400}
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-    // 软件默认打开的宽和高
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL(path.join("file:", __dirname, "index.html"));
 
-    // 把 index.html 加载到窗口
-    // 使用相对路径
-    mainWindow.loadFile('index.html');
+  //开启渲染进程中的调试模式
+  mainWindow.webContents.openDevTools();
 
-    // 打开调试工具
-    mainWindow.webContents.openDevTools();
+  console.log(path.join("file:", __dirname, "index.html"));
 
-    // 加载 index.html 的第二种方式
-    // 使用绝对路径
-    mainWindow.loadURL(path.join('file:', __dirname, 'index.html'));
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
 
+app.on("ready", createWindow);
 
-    // 监听窗口关闭时间
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-})
+// 当所有的窗口被关闭后退出应用   Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+
+  // 对于OS X系统，应用和相应的菜单栏会一直激活直到用户通过Cmd + Q显式退出
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+//macos
+app.on("activate", () => {
+  // 对于OS X系统，当dock图标被点击后会重新创建一个app窗口，并且不会有其他
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
